18c45f0c8fae5b6bd104d0b00129e1ed8a79ca26
Serhii <sirozhka.v@gmail.com>
2023-07-27 15:34:06 +0200

    UI component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,11 +1,13 @@
 import { OrbitControls, ScrollControls } from '@react-three/drei';
 import { Office } from './Office';
+import { Overlay } from './Overlay';
 const Experience = () => {
   return (
     <>
       <ambientLight intensity={1} />
       <OrbitControls enableZoom={false} />
       <ScrollControls pages={3} damping={0.25}>
+        <Overlay />
         <Office />
       </ScrollControls>
     </>
