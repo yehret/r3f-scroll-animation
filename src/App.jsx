c4fbf62078e291358c2be1cf0df70bda918c8bd2
Serhii <sirozhka.v@gmail.com>
2023-07-27 15:28:32 +0200

    gsap 3d model animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 import { Canvas } from '@react-three/fiber';
-import Experience from './Experience';
+import Experience from './components/Experience';
 
 function App() {
   return (
     <>
-      <Canvas>
+      <Canvas
+        camera={{
+          fov: 64,
+          position: [2.3, 1.5, 2.3],
+        }}>
         <Experience />
       </Canvas>
     </>
